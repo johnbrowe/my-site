bba95ecd4a06535a95ec75ed76d8cb869fae5001
johnbrowe <john.andreassen83@gmail.com>
2015-11-15 15:35:50 +0100

    Improved gulp workflow

--- code/js/general.js
+++ code/js/general.js
@@ -1,12 +1,9 @@
 new Vue({
+
     el: '#notes',
+
     data: {
-        todos: [
-            { text: 'Learn JavaScript' },
-            { text: 'Learn Vue.js' },
-            { text: 'Build Something Awesome' }
-        ],
-        message: "hello",
+
         notes: null
 
     },
@@ -22,6 +19,10 @@ new Vue({
 
     methods: {
 
+        /**
+         * Get notes from JSON API
+         */
+
         fetchData: function () {
 
             // This for nested functions
@@ -29,13 +30,16 @@ new Vue({
 
             // Get notes via ajax
             $.ajax({
+
                 url: "notes",
                 context: document.body
+
             }).done(function (data) {
 
                 // Add json to data
                 self.notes = data;
                 console.log(self.notes);
+
             });
 
         }
@@ -43,66 +47,3 @@ new Vue({
     }
 
 });
-
-/**
- * General.js
- * Custom JavScript
- */
-/*
-new Vue({
-
-    el: '#notes',
-
-    data: {
-
-        message: "hello",
-        notes: null
-
-    },
-
-    created: function () {
-
-        // Get notes
-        this.fetchData()
-
-    },
-
-    filters: {},
-
-    methods: {
-        fetchData: function () {
-
-            // This for nested functions
-            var self = this;
-
-            // Get notes via ajax
-            $.ajax({
-                url: "notes",
-                context: document.body
-            }).done(function (data) {
-
-                // Add json to data
-                self.notes = data;
-                console.log(self.notes);
-            });
-
-        }
-    }
-
-});
-*/
-//$.ajax({
-//    url: "notes",
-//    context: document.body
-//}).done(function(data) {
-//
-//
-//    for (var key in data) {
-//        data[key].forEach(function(data){
-//            console.log(data);
-//            $('body').append(data);
-//
-//        });
-//    }
-//
-//});
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,18 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var sass = require('gulp-sass');
 
-gulp.task('default', function() {
-    return gulp.src([
+// Default
+gulp.task('default', ['js', 'sass']);
+
+// Watch
+gulp.task('watch', function () {
+    gulp.watch(['./code/js/*.js', './code/scss/*.scss'], ['default']);
+});
+
+// Javacript
+gulp.task('js', function() {
+    gulp.src([
         './node_modules/jquery/dist/jquery.min.js',
         './node_modules/vue/dist/vue.min.js',
         './code/js/*.js'
@@ -10,10 +20,17 @@ gulp.task('default', function() {
         .pipe(concat('general.js'))
         .pipe(gulp.dest('./public/'));
 
-    console.log('Compiled');
+    console.log('JavaScript compiled');
+
 });
 
+// SASS
+gulp.task('sass', function () {
 
-gulp.task('watch', function () {
-    gulp.watch('./code/js/*.js', ['default']);
+    gulp.src('./code/scss/*.scss')
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(gulp.dest('./public/css'));
+
+    console.log("Sass compiled");
+
 });
