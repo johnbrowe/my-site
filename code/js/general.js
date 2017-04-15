f588cd565224ff14fe07b162732ea6382c1b8f7a
John Høj Andreassen <jha@obviux.dk>
2017-04-15 12:18:39 +0200

    Add ES 6 syntax

--- code/js/general.js
+++ code/js/general.js
@@ -1,3 +1,5 @@
+import Vue from "vue"
+
 Vue.config.devtools = true;
 
 window.Event = new Vue();
@@ -14,26 +16,25 @@ Vue.component('note', {
 
     props: ['text'],
     
-    template: '<div class="modal" :class="{ active: store.showModal }">' +
-                '<div v-if="store.note" class="modal__box">' +
-                    '<a @click="closeModal" title="Close" class="close">X</a>' +
-                    '<div v-html="store.note"></div>' +
-                '</div>' +
-              '</div>',
+    template:
+        `<div class="modal" :class="{ active: store.showModal }">
+            <div v-if="store.note" class="modal__box">
+                <a @click="closeModal" title="Close" class="close">X</a>
+                <div v-html="store.note"></div>
+            </div>
+          </div>`,
     
-    data: function(){
+    data () {
         return {
             store: store
         }        
     },
 
     methods: {
-
-        closeModal: function(){
+        closeModal () {
             this.store.showModal = false;
             this.store.note = null;
         }
-
     }
 
 });
@@ -43,15 +44,14 @@ Vue.component('note', {
  * Notes component
  */
 Vue.component('notes', {
-
     template: 
-    '<ul>' +
-        '<li v-for="(note, index) in notes" @click="openModal(note)">' +
+    '<ul class="notes__list">' +
+        '<li class="notes__list__items" v-for="(note, index) in notes" @click="openModal(note)">' +
             '<div class="text" v-html="note" ></div>' +
         '</li>' +
     '</ul>',
     
-    data: function(){
+    data () {
         return {
             notes: null,
             url: document.URL,
@@ -59,37 +59,30 @@ Vue.component('notes', {
         }    
     },
 
-    created: function () {
-
+    created () {
         // Get notes
         this.ajax()
-        
     },
 
 
     methods: {
-
         /**
          * Get notes from JSON API
          */
         ajax: function() {
             // This for nested functions
             var self = this;
-
             var request = new XMLHttpRequest();
-    
             request.open('GET', '/notes', true);
 
             request.onload = function() {
-                if (request.status >= 200 && request.status < 400) {            
-    
+                if (request.status >= 200 && request.status < 400) {
                     // Success!
                     var resp = request.responseText;
     
                     // Add json to data
                     self.notes = JSON.parse(resp);
                     console.dir(self.notes);
-                        
                 } else {
                     // We reached our target server, but it returned an error
                 }
@@ -101,10 +94,9 @@ Vue.component('notes', {
             };
 
             request.send();
-
         },
 
-        openModal: function(note){
+        openModal (note) {
             console.log(note);
             this.store.note = note;
             this.store.showModal = true;
